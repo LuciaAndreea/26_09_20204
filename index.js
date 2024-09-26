4e00a6ebd5c169c431281b5eb6d9597fdcd0674e
Lucia <luciaandreeaursu@yahoo.com>
2024-09-26 19:34:54 +0300

    add methods of string

--- index.js
+++ index.js
@@ -14,3 +14,44 @@ console.log(email);
 // constanta declarata cu valoare
 const pi = 3.14;
 //pi = 3.12;  --INTERZISA RE-ATRIBUIREA DE VALORI UNEI CONSTANTE
+console.log(pi);
+
+// Operatii aritmetice 
+
+//adunare
+const sumResult = 12 + 32;
+console.log("sumResult: ", sumResult);
+
+//scadere
+const differenceResult = 33-71;
+console.log("difference: ",differenceResult);
+
+//String
+let password = "secret";
+console.log("password: ",password);
+
+//accesarea unui caracter dintr-un string
+let secondLetterOfPassword = password[1];
+console.log("a doua litera: ",secondLetterOfPassword);
+
+//concatenare stringuri:
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " "+ lastName;
+console.log("fullname: ",fullName)
+
+//lungimea stringului
+let myString = "hello people";
+let myStringLength = myString.length;
+console.log("lungime myString: ",myStringLength);
+
+//metode string
+let baseString = "Base";
+let upper = baseString.toUpperCase();
+let lower = baseString.toLowerCase();
+console.log("base: ",baseString);
+console.log("upperCase: ",upper);
+console.log("lowerCase: ",lower);
+
+let substringOfBase = baseString.substring(0, 2);
+console.log("substringOfBase: ",substringOfBase);
