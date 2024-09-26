47c6f6e8269e7fa10906c33fe5279bb3154a0ec4
Lucia <luciaandreeaursu@yahoo.com>
2024-09-26 18:43:01 +0300

    add variables

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+// variabila declarata si apoi atribuita valoare separat
+let userName;
+userName = "Monica";
+console.log(userName);
+userName = "_monica";
+console.log(userName);
+
+// variabila declarata si initiata cu o valoare intr-o linie 
+let email = "user@test.com";
+console.log(email);
+
+// constanta declarata cu valoare
+const pi = 3.14;
+//pi = 3.12;  --INTERZISA RE-ATRIBUIREA DE VALORI UNEI CONSTANTE
